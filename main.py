98919a597134f847719359a75272c3a0abcded62
ZigaE <zigaen@gmail.com>
2020-05-25 07:37:12 +0200

    excercise - jinja Changes

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
+
 
 @app.route("/about")
 def about_me():
@@ -23,6 +29,10 @@ def fakebook():
 @app.route("/boogle")
 def boogle():
     return render_template("boogle.html")
+
+@app.route("/base")
+def base():
+    return render_template("base.html")
 
 if __name__ == '__main__':
     app.run()
