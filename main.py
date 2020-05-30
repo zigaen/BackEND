b9ae7c3350a535ea8c05a519024eaf559473e7be
ZigaE <zigaen@gmail.com>
2020-05-30 20:22:31 +0200

    Popravljen gitignore.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 import datetime
 
 app = Flask(__name__)
@@ -13,9 +13,24 @@ def index():
     return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
 
-@app.route("/about-me")
-def about_me():
-    return render_template("about.html")
+@app.route("/about-me", methods=["GET", "POST"])
+def about():
+    if request.method == "GET":
+        user_name = request.cookies.get("user_name")
+        return render_template("about.html", name=user_name)
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
+
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+
+        response = make_response(render_template("success.html"))
+        response.set_cookie("user_name", contact_name)
+
+        return response
 
 
 @app.route("/portfolio")
@@ -33,6 +48,8 @@ def boogle():
 @app.route("/base")
 def base():
     return render_template("base.html")
+
+
 
 if __name__ == '__main__':
     app.run()
